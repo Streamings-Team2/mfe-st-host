9d70eb679507f8d087f636f0f9e1d6b568727ec4
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-11-07 17:55:20 -0500

    Configuracion inicial

---
+++ src/components/InitComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const InitComponent = () => {
+  return (
+  <>
+  <h1>INIt 404</h1>
+  </>
+
+  )
+}
+
+export default InitComponent
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+const deps = require("./package.json").dependencies;
+
+const printCompilationMessage = require('./compilation.config.js');
+
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: "http://localhost:4200/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 4200,
+    historyApiFallback: true,
+    watchFiles: [path.resolve(__dirname, 'src')],
+    onListening: function (devServer) {
+      const port = devServer.server.address().port
+
+      printCompilationMessage('compiling', port)
+
+      devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
+        setImmediate(() => {
+          if (stats.hasErrors()) {
+            printCompilationMessage('failure', port)
+          } else {
+            printCompilationMessage('success', port)
+          }
+        })
+      })
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "mfe_st_host",
+      filename: "remoteEntry.js",
+      remotes: {
+        mfe_st_common: 'mf_st_common@http://localhost:4210/remoteEntry.js',
+        mfe_st_utils: 'mfe_st_utils@http://localhost:4211/remoteEntry.js',
+        mfe_st_errors: 'mfe_st_error@http://localhost:4212/remoteEntry.js'
+      },
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+    new Dotenv()
+  ],
+});
